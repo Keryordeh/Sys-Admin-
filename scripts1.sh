1b88a0a86167abf8848203f99b8fde490c446bf8
nkeryordeh <nkeryordeh@gmail.com>
2023-12-14 13:59:11 -0500

    New Scripts

---
+++ scripts1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+##Example of input/output 
+a=`hostname`
+echo 
+echo "Hello,this server's name is $a"
+echo
+echo "What is your name?"
+read b 
+echo 
+echo Hello $b 
+
---
+++ scripts3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#For/Loop
+#Original
+ 
+for i in 123 
+do 
+	echo "Welcome $i times" 
+done 
+echo 
+for x in add commit push 
+do 
+	echo "A commonly used git command is $x" 
+done 
+
